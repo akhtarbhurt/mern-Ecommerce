5031d64876103659a4b67010fe1417a560cfa3ec
akhtar <bhurtsahab786521@gmail.com>
2024-07-19 10:07:02 +0500

    code has been updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,4 +9,11 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false, limit: "16kb"}))
 app.use(cookieParser())
 
+import banner from "./routes/banner.routes"
+import { errorHandler } from "./middleware/errorHandler.middleware"
+
+app.use("/api", banner )
+app.use(errorHandler)
+
+
 export {app}
